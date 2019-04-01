118176832958764ca4dd8b25db99ced33b63be3f
6LYTH3 <ST4R13UCK@gmail.com>
2019-04-01 21:12:47 +0700

    #4 Signing transactions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 const { Blockchain, Transaction } = require('./blockchain')
+const EC = require('elliptic').ec
+const ec = new EC('secp256k1')
+
+// Private key from KeyGenerator.js
+const myKey = ec.keyFromPrivate('55611467bb934da8d8cfb156be546514c1cfee1ace8593c6a483e63c4c1ddacd')
+const myWalletAddress = myKey.getPublic('hex')
+
 
 let jibbyCoin = new Blockchain()
-jibbyCoin.createTransaction(new Transaction('address1', 'address2', 100))
-jibbyCoin.createTransaction(new Transaction('address2', 'address1', 50))
+const tx1 = new Transaction(myWalletAddress, 'public key goes here', 10)
+tx1.signTransaction(myKey)
+jibbyCoin.addTransaction(tx1)
+
 
 console.log('\nStaring the miner...')
-jibbyCoin.minePendingTransactions('blythe-address')
+jibbyCoin.minePendingTransactions(myWalletAddress)
 
-console.log('\nBalance of Blythe is ', jibbyCoin.getBalanceOfAddress('blythe-address'))
+console.log('\nBalance of Blythe is ', jibbyCoin.getBalanceOfAddress(myWalletAddress))
+console.log('Is chain valid?', jibbyCoin.isChainValid())
 
-console.log('\nStaring the miner again...')
-jibbyCoin.minePendingTransactions('blythe-address')
-
-console.log('\nBalance of Blythe is ', jibbyCoin.getBalanceOfAddress('blythe-address'))
+console.log(JSON.stringify(jibbyCoin.getAllTransactionForWallet(myKey), null, 2))
